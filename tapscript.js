60eece75d8460448347abbb632774360f20a8521
vinodmp4 <vinodmp4@live.com>
2021-12-27 21:12:21 +0530

    updated script- added speech function, added new way to manage data

--- tapscript.js
+++ tapscript.js
@@ -1,7 +1,28 @@
+var data = [];
+
 function senddata(){
 	var text = document.getElementById('inputbox').value;
-	document.getElementById('chatarea').innerHTML += '<div id="me">'+text+'</div></br>';
-	document.getElementById('chatarea').innerHTML += '<div id="you">hi</div></br>';
+	data.push(['me',text])
+	document.getElementById('chatarea').innerHTML ='</br></br></br></br><center>Tap anywhere to Read</br>Hold anywhere to record</center></br>';
+	for(var i=0;i<data.length;i++){
+		if (data[i][0]==='me'){
+			document.getElementById('chatarea').innerHTML += '<div id="me">'+data[i][1]+'</div></br>';
+		}
+		else{
+			document.getElementById('chatarea').innerHTML += '<div id="you">'+data[i][1]+'</div></br>';
+		}
+	}
+}
+
+function readall(){
+	for(var i=0;i<data.length;i++){
+		if (data[i][0]==='me'){
+			window.speechSynthesis.speak(new SpeechSynthesisUtterance('you said '+data[i][1]));
+		}
+		else{
+			window.speechSynthesis.speak(new SpeechSynthesisUtterance('reply was '+data[i][0]));
+		}
+	}
 }
 
 
