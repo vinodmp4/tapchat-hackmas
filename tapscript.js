b76dc1c94894304f77c32889626b91a5bbeba206
vinodmp4 <vinodmp4@live.com>
2021-12-28 00:02:51 +0530

    test reply was added speech recognition testing initiated

--- tapscript.js
+++ tapscript.js
@@ -1,8 +1,12 @@
 var data = [];
-
+var time1 = 0;
+var time2 = 0;
+var recog = 0;
 function senddata(){
 	var text = document.getElementById('inputbox').value;
-	data.push(['me',text])
+	document.getElementById('inputbox').value = '';
+	data.push(['me',text]);
+	data.push(['you','test reply']);
 	document.getElementById('chatarea').innerHTML ='</br></br></br></br><center>Tap anywhere to Read</br>Hold anywhere to record</center></br>';
 	for(var i=0;i<data.length;i++){
 		if (data[i][0]==='me'){
